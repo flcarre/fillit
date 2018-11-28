4f8820d86498170cbda9c204a0ff53cc07e1aa9b
flcarre <flcarre@student.42.fr>
2018-11-28 23:48:30 +0100

    fastpush

--- ft_binchar_to_short.c
+++ ft_binchar_to_short.c
@@ -6,21 +6,11 @@
 /*   By: flcarre <flcarre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 22:56:41 by flcarre           #+#    #+#             */
-/*   Updated: 2018/11/28 23:47:21 by flcarre          ###   ########.fr       */
+/*   Updated: 2018/11/28 23:48:24 by flcarre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static long	ft_paaw(long x, unsigned int n)
-{
-	long	r;
-
-	if (!n)
-		return (1);
-	r = 1;
-	while (n--)
-		r = r * x;
-	return (r);
-}
+#include "fillit.h"
 
 void	ft_strtobin2(char *tetrimino, unsigned short *bin, int len)
 {
@@ -32,6 +22,6 @@ void	ft_strtobin2(char *tetrimino, unsigned short *bin, int len)
 		if (tetrimino[len] == '.')
 			i++;
 		else if (tetrimino[len] == '#')
-			*bin += (unsigned short)ft_paaw(2, i++);
+			*bin += (unsigned short)ft_paw(2, i++);
 	}
 }
